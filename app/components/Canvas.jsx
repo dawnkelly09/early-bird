0e4adab2ddb4093f0b346b58d9f315dc22bdfda0
Dawn Kelly <dawnkelly09@gmail.com>
2024-05-16 12:42:08 -0400

    adds background and robin(0,0) rendering at start of game play

--- app/components/Canvas.jsx
+++ app/components/Canvas.jsx
@@ -5,6 +5,20 @@ function Canvas() {
   const canvasWidth = 600;
   const canvasHeight = 600;
   const ctx = canvasRef.current?.getContext('2d');
+  // define intial (x,y) position of robin
+    let robinX = 0;
+    let robinY = 0;
+
+  const drawRobin = (robinX, robinY) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const robinImg = new Image();
+    robinImg.src = robinImg.src = "https://cyan-interesting-takin-110.mypinata.cloud/ipfs/QmaKqEGonantGEUkEQszqoS5v6Yy3VBwZJ7f5hjHSiYCr5";
+
+    robinImg.onload = function () {
+        ctx.drawImage(robinImg, robinX, robinY, 50, 50);
+    };
+  }
 
   // Load the background image when the component mounts
     useEffect(() => {
@@ -17,7 +31,6 @@ function Canvas() {
         
             // Create a new Image object
             const background = new Image();
-            
             // Set the source of the image
             background.src = backgroundSrc;
         
@@ -29,6 +42,7 @@ function Canvas() {
 
         // Draw the background image
         drawBackground();
+        drawRobin(0, 0);
     }, [ctx]); // Trigger the effect only when the context changes
 
   return <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight}></canvas>;
